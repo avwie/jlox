09810527f71b06f45d5ecc102cf2370c5f1fa4ec
Arjan van Wieringen <arjan.van.wieringen@gmail.com>
2021-12-26 16:21:50 +0100

    Keywords and literals

--- src/main/java/nl/avwie/jlox/Scanner.java
+++ src/main/java/nl/avwie/jlox/Scanner.java
@@ -1,7 +1,9 @@
 package nl.avwie.jlox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static nl.avwie.jlox.TokenType.*;
 
@@ -13,6 +15,28 @@ public class Scanner {
     private int start = 0;
     private int current = 0;
     private int line = 1;
+
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",    AND);
+        keywords.put("class",  CLASS);
+        keywords.put("else",   ELSE);
+        keywords.put("false",  FALSE);
+        keywords.put("for",    FOR);
+        keywords.put("fun",    FUN);
+        keywords.put("if",     IF);
+        keywords.put("nil",    NIL);
+        keywords.put("or",     OR);
+        keywords.put("print",  PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super",  SUPER);
+        keywords.put("this",   THIS);
+        keywords.put("true",   TRUE);
+        keywords.put("var",    VAR);
+        keywords.put("while",  WHILE);
+    }
 
     public Scanner(String source) {
         this.source = source;
@@ -69,6 +93,8 @@ public class Scanner {
                 }
                 break;
 
+            case '"': string(); break;
+
             case ' ':
             case '\r':
             case '\t':
@@ -79,7 +105,13 @@ public class Scanner {
                 break;
 
             default:
-                JLox.error(line, "Unexpected character.");
+                if (Character.isDigit(c)) {
+                    number();
+                } else if(isAlpha(c)) {
+                    identifier();
+                }else {
+                    JLox.error(line, "Unexpected character.");
+                }
                 break;
         }
     }
@@ -108,5 +140,53 @@ public class Scanner {
     private char peek() {
         if (isAtEnd()) return '\0';
         return source.charAt(current);
+    }
+
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+
+        if (isAtEnd()) {
+            JLox.error(line, "Unterminated string.");
+            return;
+        }
+        advance();
+
+        var value = source.substring(start + 1, current -1);
+        addToken(STRING, value);
+    }
+
+    private void number() {
+        while (Character.isDigit(peek())) advance();
+
+        // fraction
+        if (peek() == '.' && Character.isDigit(peekNext())) {
+            advance();
+            while (Character.isDigit(peek())) advance();
+        }
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = IDENTIFIER;
+        addToken(type);
+    }
+
+    private boolean isAlpha(char c) {
+        return Character.isAlphabetic(c) || c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || Character.isDigit(c);
     }
 }
--- src/test/java/nl/avwie/jlox/ScannerTest.java
+++ src/test/java/nl/avwie/jlox/ScannerTest.java
@@ -33,4 +33,13 @@ class ScannerTest {
         assertEquals(TokenType.RIGHT_BRACE, tokens.get(5).type);
         assertEquals(TokenType.EOF, tokens.get(6).type);
     }
+
+    @Test
+    void scanKeywordsAndLiterals() {
+        var tokens = new Scanner("var x = \"Foobar\"").scanTokens();
+        assertEquals(TokenType.VAR, tokens.get(0).type);
+        assertEquals(TokenType.IDENTIFIER, tokens.get(1).type);
+        assertEquals(TokenType.EQUAL, tokens.get(2).type);
+        assertEquals(TokenType.STRING, tokens.get(3).type);
+    }
 }
